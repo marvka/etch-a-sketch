2165ed45a2abb6eb5118b8f93ed0261e7a3d296f
marvkal <marvink@mailbox.org>
2020-05-11 23:16:05 +0100

    Move JS to own file

---
+++ script.js
@@ -0,0 +1,16 @@
+const sketchGrid = document.querySelector("#sketchGrid");
+let gridSize = 16;
+
+for (i = 0; i < gridSize; i++) {
+  let row = document.createElement("div");
+  row.classList.add("row");
+  for (j = 0; j < gridSize; j++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.addEventListener("mouseover", (e) => {
+      cell.style.background = "black";
+    });
+    row.appendChild(cell);
+  }
+  sketchGrid.appendChild(row);
+}
