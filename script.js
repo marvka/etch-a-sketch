7b9a150801e56759afcb674d5bc7fc7bbff33d03
marvkal <marvink@mailbox.org>
2020-05-11 23:34:39 +0100

    Refactor code for building grid into separate function

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
 const sketchGrid = document.querySelector("#sketchGrid");
 let gridSize = 16;
 
-for (i = 0; i < gridSize; i++) {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  for (j = 0; j < gridSize; j++) {
-    let cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.addEventListener("mouseover", (e) => {
-      cell.style.background = "black";
-    });
-    row.appendChild(cell);
-  }
-  sketchGrid.appendChild(row);
-}
+buildGrid();
 
 function setGridColors (color) {
 
@@ -21,4 +9,20 @@ function setGridColors (color) {
 
 function deleteGrid () {
   sketchGrid.innerHTML = "";
+}
+
+function buildGrid (gridSize = 16) {
+  for (i = 0; i < gridSize; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (j = 0; j < gridSize; j++) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.addEventListener("mouseover", (e) => {
+        cell.style.background = "black";
+      });
+      row.appendChild(cell);
+    }
+    sketchGrid.appendChild(row);
+  }
 }
