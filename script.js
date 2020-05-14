91b69988f1d3252c24c1551b96de428ee34fddd7
marvkal <marvink@mailbox.org>
2020-05-14 17:58:05 +0100

    Crudely implement color darkening

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
 const sketchGrid = document.querySelector("#sketchGrid");
 const newGridButton = document.querySelector("#newGrid");
 
-function setGridColors (color) {
-
+function darkenRGBAColor(rgba) {
+  extractRGBValues(rgba);
 }
 
-function deleteGrid () {
+// TODO implement function that darkens color that has already been filled with a random color
+function setCellColor(e) {
+  const cell = e.currentTarget;
+
+  if (!cell.style.backgroundColor) {
+    cell.style.backgroundColor = getRandomColor();
+    return;
+  }
+
+  let RGBA_Array = [];
+  if (/rgba/.test(cell.style.backgroundColor)) {
+    RGBA_Array = extractRGBValues(cell.style.backgroundColor);
+  } else {
+    RGBA_Array = extractRGBValues(convertRGBtoRGBA(cell.style.backgroundColor));
+  }
+
+  if (RGBA_Array[3] > 0) {
+    cell.style.backgroundColor = `rgba(${RGBA_Array[0]}, ${RGBA_Array[1]}, ${RGBA_Array[2]}, ${RGBA_Array[3] - 0.1})`;
+  }
+
+  console.log(cell.style.backgroundColor);
+}
+
+function convertRGBtoRGBA(RGBString) {
+  let RGBAString = "";
+  RGBAString = RGBString.replace(/rgb/, "rgba");
+  RGBAString = RGBAString.replace(/\)/, ", 1)");
+  return RGBAString;
+}
+
+function extractRGBValues(RBGString) {
+  return RBGString.match(/\d+, \d+, \d+, \d+\.*\d*/)
+    .toString()
+    .split(", ");
+}
+
+function deleteGrid() {
   sketchGrid.innerHTML = "";
 }
 
-function buildGrid (gridSize = 16) {
+function buildGrid(gridSize = 16) {
   for (i = 0; i < gridSize; i++) {
     let row = document.createElement("div");
+
     row.classList.add("row");
-    row.style.height = `${1 / gridSize * 100}%`;
+    row.style.height = `${(1 / gridSize) * 100}%`;
+
     for (j = 0; j < gridSize; j++) {
       let cell = document.createElement("div");
+
       cell.classList.add("cell");
-      cell.style.width = `${1 / gridSize * 100}%`;
-      cell.addEventListener("mouseover", (e) => {
-        cell.style.background = "black";
-      });
+      cell.style.width = `${(1 / gridSize) * 100}%`;
+      cell.addEventListener("mouseover", setCellColor);
+
       row.appendChild(cell);
     }
     sketchGrid.appendChild(row);
@@ -28,16 +66,22 @@ function buildGrid (gridSize = 16) {
 }
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+  let RGBString = "rgb(";
+  for (let i = 0; i < 3; i++) {
+    if (i < 2) {
+      RGBString += `${Math.floor(Math.random() * 255)},`;
+    } else if (i < 3) {
+      RGBString += `${Math.floor(Math.random() * 255)})`;
+    }
   }
-  return color;
+
+  return RGBString;
 }
 
 newGridButton.addEventListener("click", (e) => {
-  let gridSize = prompt("How many squares per side do you want in your new Grid?");
+  let gridSize = prompt(
+    "How many squares per side do you want in your new Grid?"
+  );
   deleteGrid();
   buildGrid(gridSize);
 });
